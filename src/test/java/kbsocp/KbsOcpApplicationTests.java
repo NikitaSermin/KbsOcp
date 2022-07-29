4eec8b03b75d2d889e7222fa3b34bbf323e18890
Nikita Sermin <serminnikita@gmail.com>
2022-07-29 13:10:21 +0300

    first commit

---
+++ src/test/java/kbsocp/KbsOcpApplicationTests.java
@@ -0,0 +1,13 @@
+package kbsocp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbsOcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
